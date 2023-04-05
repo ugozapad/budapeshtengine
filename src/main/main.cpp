56e6e36f9f7a40d3e891aaecfc79a08c5fa2cdb9
Kirill Yurkin <hp1link2@gmail.com>
2023-04-06 02:05:52 +0300

    Lock several instance of application

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,3 +1,6 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 #include "engine/engine.h"
 #include "render/render.h"
 
@@ -18,8 +21,27 @@ private:
 };
 
 static Main s_main;
+static HANDLE s_locker_mutex;
 
 int Main::init(int argc, char* argv[]) {
+
+	bool createLockerMutex = true;
+
+	for (int i = 0; i < argc; i++) {
+		if (strcmp("-multinstance", argv[i]) == 0)
+			createLockerMutex = false;
+	}
+
+	if (createLockerMutex) {
+		s_locker_mutex = OpenMutexA(READ_CONTROL, FALSE, "execution_locker_mutex");
+		if (s_locker_mutex) {
+			MessageBoxA(nullptr, "Failed to open second instance.", "Error", MB_OK | MB_ICONERROR);
+			return 2;
+		} else {
+			s_locker_mutex = CreateMutexA(NULL, FALSE, "execution_locker_mutex");
+		}
+	}
+
 	m_engine = new Engine();
 	m_engine->init();
 
