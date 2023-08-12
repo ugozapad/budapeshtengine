7e066dabe8f8f77971d60c33ffa8e1cdd2bf37b0
Kirill Yurkin <ugo0zapad@yandex.ru>
2023-08-13 02:14:24 +0300

    Editor stuff

--- src/editor/editorsystem.cpp
+++ src/editor/editorsystem.cpp
@@ -3,7 +3,8 @@
 #include "engine/engine_api.h"
 #include "engine/allocator.h"
 #include "engine/engine.h"
-
+#include "engine/camera.h"
+#include "engine/objectfactory.h"
 #include "editor_shared.h"
 
 #include <imgui.h>
@@ -23,6 +24,16 @@ public:
 
 	void update(float dt);
 	void render();
+
+	void renderMenu();
+
+private:
+	// Windows
+	bool m_bHelpWindowOpened;
+	bool m_bAboutWindowOpened;
+	bool m_bCameraDebugWindowOpened;
+	bool m_bDebugConsoleWindowOpened;
+	bool m_bEntityCreationWindowOpened;
 };
 
 // Global instance of editor
@@ -31,6 +42,11 @@ static IEditorSystem* g_pEditorSystem;
 EditorSystem::EditorSystem()
 {
 	g_pEditorSystem = this;
+	m_bHelpWindowOpened = false;
+	m_bAboutWindowOpened = false;
+	m_bCameraDebugWindowOpened = false;
+	m_bDebugConsoleWindowOpened = false;
+	m_bEntityCreationWindowOpened = false;
 }
 
 EditorSystem::~EditorSystem()
@@ -91,6 +107,8 @@ void EditorSystem::update(float dt)
 	ImGui::NewFrame();
 
 	// Draw ...
+	renderMenu();
+
 	//ImGui::ShowDemoWindow((bool*)true);
 }
 
@@ -113,6 +131,158 @@ void EditorSystem::render()
 		ImGui::RenderPlatformWindowsDefault();
 		SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
 	}
+}
+
+void EditorSystem::renderMenu()
+{
+
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ImGui::MenuItem("New");
+			ImGui::MenuItem("Open...");
+			ImGui::MenuItem("Save");
+			ImGui::MenuItem("Save As...");
+			ImGui::Separator();
+			ImGui::MenuItem("Exit");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Edit"))
+		{
+			ImGui::MenuItem("Undo");
+			ImGui::MenuItem("Redo");
+			ImGui::Separator();
+			ImGui::MenuItem("Copy");
+			ImGui::MenuItem("Cut");
+			ImGui::MenuItem("Paste");
+			ImGui::Separator();
+			ImGui::MenuItem("Delete");
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Tools"))
+		{
+			if (ImGui::MenuItem("Entity Creation"))
+				m_bEntityCreationWindowOpened = true;
+
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Debug"))
+		{
+			if (ImGui::MenuItem("Camera"))
+				m_bCameraDebugWindowOpened = true;
+			if (ImGui::MenuItem("Console"))
+				m_bDebugConsoleWindowOpened = true;
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::BeginMenu("Help"))
+		{
+			if (ImGui::MenuItem("Help"))
+				m_bHelpWindowOpened = true;
+			ImGui::Separator();
+			if (ImGui::MenuItem("About"))
+				m_bAboutWindowOpened = true;
+			ImGui::EndMenu();
+		}
+
+		ImGui::EndMainMenuBar();
+	}
+
+	if (m_bAboutWindowOpened)
+	{
+		if (ImGui::Begin("About", &m_bAboutWindowOpened))
+		{
+			ImGui::Text("Budapest Engine 0.01");
+			ImGui::Text("Build Timestamp: %s", __TIMESTAMP__);
+		}
+		ImGui::End();
+	}
+
+	if (m_bHelpWindowOpened)
+	{
+		if (ImGui::Begin("Help", &m_bHelpWindowOpened))
+		{
+			ImGui::Text("No help");
+		}
+		ImGui::End();
+	}
+
+	if (m_bCameraDebugWindowOpened)
+	{
+		if (ImGui::Begin("Camera Debug", &m_bCameraDebugWindowOpened))
+		{
+			ImGui::InputFloat3("Position", reinterpret_cast<float*>(&g_camera.getPosition()));
+			ImGui::InputFloat3("Direction", reinterpret_cast<float*>(&g_camera.getDirection()));
+		}
+		ImGui::End();
+	}
+
+	if (m_bEntityCreationWindowOpened)
+	{
+		if (ImGui::Begin("Entity Creation", &m_bEntityCreationWindowOpened))
+		{
+			Array<objectCreationInfo_t> objects;
+			g_object_factory->getObjectCreationInfos(objects);
+
+			for (auto it : objects)
+			{
+				ImGui::Text("%s", it.classname);
+			}
+		}
+
+		ImGui::End();
+	}
+
+#if 0
+	{
+		static char tmp[256];
+		if (m_bDebugConsoleWindowOpened)
+		{
+			if (ImGui::Begin(
+				"Debug Console",
+				&m_bDebugConsoleWindowOpened
+			))
+			{
+				if (ImGui::BeginChild(
+					"LogFrame",
+					ImVec2(0.f, ImGui::GetWindowSize().y - 64.f),
+					true
+				))
+				{
+					Logger::iterator I = Logger::begin(), E = Logger::end();
+					for (; I != E; ++I)
+						ImGui::TextUnformatted((*I).c_str());
+					ImGui::SetScrollHereY();
+				}
+				ImGui::EndChild();
+
+				if (ImGui::InputTextEx(
+					"##CommandInput",
+					"Type command here and press Enter",
+					tmp,
+					sizeof(tmp),
+					ImVec2(-1.f, 0.f),
+					ImGuiInputTextFlags_EnterReturnsTrue
+				))
+				{
+					Logger::msg("> %s", tmp);
+					CmdArgs args(tmp);
+					if (g_cmdManager.hasCmd(args.getArgv(0)))
+						g_cmdManager.executeNow(tmp);
+					else
+						Logger::msg("! Unreconized command %s", tmp);
+					tmp[0] = '\0';
+				}
+			}
+			ImGui::End();
+		}
+		else tmp[0] = '\0';
+	}
+#endif
 }
 
 // Internal singleton
