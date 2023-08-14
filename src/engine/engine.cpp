1829858f629b2ccb282fbe873d12745a95edba0f
DForgotten <58083488+OneForgottenMan@users.noreply.github.com>
2023-08-14 14:01:32 +0300

    Removed comments

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -147,8 +147,6 @@ void Engine::createRenderDevice(const char* devicename)
 		{
 			pRenderDevice = createRenderDeviceProc();
 		}
-
-		//CloseHandle(hRenderDeviceLib);
 	}
 
 	ASSERT(pRenderDevice && "Failed to load render device lib");
@@ -168,9 +166,6 @@ void Engine::createSoundSystem(const char* soundname)
 		{
 			pSoundSystem = createSoundSystemProc();
 		}
-		// Dima : Do we need to close it? 
-		// Kirill: No
-		//CloseHandle(hSoundSystemLib);
 	}
 
 	ASSERT(pSoundSystem && "Failed to load sound sys. Missing dll or OpenAL installation");
